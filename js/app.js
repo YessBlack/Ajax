9cd274b661209fe7b2a0f8bcdae16dbb5fcbcf37
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2022-05-05 17:36:39 -0500

    asyn - await fetch nasa

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const API = 'https://api.nasa.gov/planetary/apod?api_key=s2ElE2Pwht59b3Z3VTGtAJw0NUiOoHhqLhWciJr1'
 
-fetch(API)
+
+/*fetch(API)
   .then(response => response.ok ? response.json() : Promise.reject(response))
   .then(json => {
     console.log(json)
@@ -12,3 +12,25 @@ fetch(API)
                         `
   })
   .catch(error => console.log(error))
+  */
+
+  const API = 'https://api.nasa.gov/planetary/apod?api_key=s2ElE2Pwht59b3Z3VTGtAJw0NUiOoHhqLhWciJr1'
+  const fetchData = async () => {
+      try {
+        const response = await fetch(API)
+        const json = await response.json()
+
+        if(!response.ok) throw {status:response.status,statusText:response.statusText}
+
+        const $card = document.getElementById('box-card')
+        $card.innerHTML = `
+                            <img src="${json.url}" alt="${json.hdurl}">
+                            <h3>${json.title}</h3>
+                            <p>${json.explanation}</p>
+                        `
+      }catch(error) {
+
+      }
+  } 
+
+fetchData()
